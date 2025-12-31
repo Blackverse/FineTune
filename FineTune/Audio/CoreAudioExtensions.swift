3a28d3494536c9372bf4ee23f06f6caace063a5b
Ronit Singh <code.rs2001@gmail.com>
2025-12-31 18:08:44 -0500

    feat: add Core Audio extensions for property access

---
+++ FineTune/Audio/CoreAudioExtensions.swift
@@ -0,0 +1,95 @@
+// FineTune/Audio/CoreAudioExtensions.swift
+import AudioToolbox
+import Foundation
+
+// MARK: - AudioObjectID Extensions
+
+extension AudioObjectID {
+    static let unknown = AudioObjectID(kAudioObjectUnknown)
+
+    var isValid: Bool { self != Self.unknown }
+}
+
+extension AudioObjectID {
+    func read<T>(_ selector: AudioObjectPropertySelector, scope: AudioObjectPropertyScope = kAudioObjectPropertyScopeGlobal, defaultValue: T) throws -> T {
+        var address = AudioObjectPropertyAddress(mSelector: selector, mScope: scope, mElement: kAudioObjectPropertyElementMain)
+        var size = UInt32(MemoryLayout<T>.size)
+        var value = defaultValue
+        let err = AudioObjectGetPropertyData(self, &address, 0, nil, &size, &value)
+        guard err == noErr else { throw NSError(domain: NSOSStatusErrorDomain, code: Int(err)) }
+        return value
+    }
+
+    func readBool(_ selector: AudioObjectPropertySelector) throws -> Bool {
+        let value: UInt32 = try read(selector, defaultValue: 0)
+        return value != 0
+    }
+
+    func readString(_ selector: AudioObjectPropertySelector) throws -> String {
+        var address = AudioObjectPropertyAddress(mSelector: selector, mScope: kAudioObjectPropertyScopeGlobal, mElement: kAudioObjectPropertyElementMain)
+        var size: UInt32 = 0
+        var err = AudioObjectGetPropertyDataSize(self, &address, 0, nil, &size)
+        guard err == noErr else { throw NSError(domain: NSOSStatusErrorDomain, code: Int(err)) }
+
+        var cfString: CFString = "" as CFString
+        err = AudioObjectGetPropertyData(self, &address, 0, nil, &size, &cfString)
+        guard err == noErr else { throw NSError(domain: NSOSStatusErrorDomain, code: Int(err)) }
+        return cfString as String
+    }
+}
+
+// MARK: - System Device Helpers
+
+extension AudioDeviceID {
+    static func readDefaultSystemOutputDevice() throws -> AudioDeviceID {
+        try AudioObjectID(kAudioObjectSystemObject).read(
+            kAudioHardwarePropertyDefaultSystemOutputDevice,
+            defaultValue: AudioDeviceID.unknown
+        )
+    }
+
+    func readDeviceUID() throws -> String {
+        try readString(kAudioDevicePropertyDeviceUID)
+    }
+}
+
+// MARK: - Process List
+
+extension AudioObjectID {
+    static func readProcessList() throws -> [AudioObjectID] {
+        var address = AudioObjectPropertyAddress(
+            mSelector: kAudioHardwarePropertyProcessObjectList,
+            mScope: kAudioObjectPropertyScopeGlobal,
+            mElement: kAudioObjectPropertyElementMain
+        )
+        var size: UInt32 = 0
+        var err = AudioObjectGetPropertyDataSize(AudioObjectID(kAudioObjectSystemObject), &address, 0, nil, &size)
+        guard err == noErr else { throw NSError(domain: NSOSStatusErrorDomain, code: Int(err)) }
+
+        let count = Int(size) / MemoryLayout<AudioObjectID>.size
+        var objectIDs = [AudioObjectID](repeating: .unknown, count: count)
+        err = AudioObjectGetPropertyData(AudioObjectID(kAudioObjectSystemObject), &address, 0, nil, &size, &objectIDs)
+        guard err == noErr else { throw NSError(domain: NSOSStatusErrorDomain, code: Int(err)) }
+        return objectIDs
+    }
+
+    func readProcessPID() throws -> pid_t {
+        try read(kAudioProcessPropertyPID, defaultValue: pid_t(0))
+    }
+
+    func readProcessIsRunning() -> Bool {
+        (try? readBool(kAudioProcessPropertyIsRunning)) ?? false
+    }
+
+    func readProcessBundleID() -> String? {
+        try? readString(kAudioProcessPropertyBundleID)
+    }
+}
+
+// MARK: - Audio Tap
+
+extension AudioObjectID {
+    func readAudioTapStreamBasicDescription() throws -> AudioStreamBasicDescription {
+        try read(kAudioTapPropertyFormat, defaultValue: AudioStreamBasicDescription())
+    }
+}
